fe24a3b6147d12c6edc3ef5bfcaa2aa03f45f376
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2026-06-12 23:31:46 +0200

    checking data drift

--- src/pipeline/drift_check.py
+++ src/pipeline/drift_check.py
@@ -1,6 +1,79 @@
-from prefect import flow, task
+from dotenv import load_dotenv
+from evidently import Dataset, DataDefinition, Report
+from evidently.presets import DataDriftPreset, DataSummaryPreset
+import os
+import pandas as pd
+from prefect import flow, task, get_run_logger
 from prefect.deployments import run_deployment
-import time
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+load_dotenv()
+logger = get_run_logger()
+engine = create_engine(f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}")
+
+
+@task(name="read_data_from_db", retries=3, retry_delay_seconds=10)
+def read_data_from_db():
+    try:
+        logger.info("Fetching the last 24 rows of sensor data from the Database...")
+        with Session(engine) as session:
+            result = session.execute(f"""
+                SELECT * FROM {os.getenv('TABLE_NAME')}
+                WHERE timestamp >= NOW() - INTERVAL '24 HOURS'
+                ORDER BY timestamp DESC
+                LIMIT 24;
+            """)
+            data = result.fetchall()
+        logger.info("Data fetched successfully from the database.")
+        return data
+    except Exception as e:
+        logger.error(f"Error fetching data from the database: {e}")
+        raise
+
+@task(name="converting_data_to_pandas", retries=3, retry_delay_seconds=10)
+def converting_data_to_pandas(data):
+    try:
+        logger.info("Converting fetched data to Dataframe...")
+        pd_data = pd.DataFrame(data)
+        logger.info("Data converted to Dataframe successfully.")
+        return pd_data
+    except Exception as e:
+        logger.error(f"Error converting data to Dataframe: {e}")
+        raise
+
+@task(name="check_for_drift")
+def drift_check(data):
+    try:
+        logger.info("Checking for drift in data...")
+        
+        schema = DataDefinition(
+            numerical_features=["SO2", "NO2", "CO", "O3", "TEMP", "PRES", "DEWP", "WSPM"],
+            categorical_features=["wd"]
+        )
+        
+        eval_data = Dataset.from_pandas(
+            data, data_definition=schema
+        )
+
+        report = Report([
+            DataDriftPreset(),
+            DataSummaryPreset()
+        ])
+
+        my_eval = report.run(reference_data=None, current_data=eval_data)
+        report.save_html("drift_report.html")
+        logger.info("Drift check completed successfully. Report saved as 'drift_report.html'.")
+
+    except Exception as e:
+        logger.error(f"Couldn't check for drift due to: {e}")
+
+@task(name="check_model_performance")
+def model_performance_check():
+    logger.info("Checking model performance...")
+    # (Your code to check model performance using MLflow metrics)
+    logger.info("Model performance check completed successfully.")
 
 @flow(name="Flow-2-Daily-Drift")
 def daily_drift():
